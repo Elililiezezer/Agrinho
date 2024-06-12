a3425be66f0f52d2f9e1ea544b00d97316312c2a
Eliézer <160596699+Elililiezezer@users.noreply.github.com>
2024-06-11 22:28:44 -0300

    ,

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var x = 0
 var y = 0
 window.addEventListener("keydown",function(event){
     var tecla = event.keyCode
+    this.alert(tecla)
     if ( tecla == "68"){
         x = x + 10
         this.document.getElementById("object").style.left=x+"px"

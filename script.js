2c578c82c0f18d02d748af550d2b0e28682b2035
Eliézer <160596699+Elililiezezer@users.noreply.github.com>
2024-06-11 22:28:13 -0300

    kkk

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var x = 0
 var y = 0
 window.addEventListener("keydown",function(event){
     var tecla = event.keyCode
-    this.alert(tecla)
     if ( tecla == "68"){
         x = x + 10
         this.document.getElementById("object").style.left=x+"px"
     }
     if ( tecla == "87"){
         y = y - 10
+        this.document.getElementById("object").style.top=y+"px"
+    }
+    if ( tecla == "83"){
+        y = y + 10
         this.document.getElementById("object").style.top=y+"px"
     }
 })

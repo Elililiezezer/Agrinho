49f97342aba3d9bd1182accb04b8d642c53187db
Eliézer <160596699+Elililiezezer@users.noreply.github.com>
2024-06-16 21:30:41 -0300

    mudei tds as var object

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ window.addEventListener("keydown",function(event){
     var tecla = event.keyCode
     if ( tecla == "68"){
         x = x + 10
-        this.document.getElementById("object").style.left=x+"px"
+        this.document.getElementById("--objet").style.left=x+"px"
     }
     if ( tecla == "87"){
         y = y - 10
-        this.document.getElementById("object").style.top=y+"px"
+        this.document.getElementById("--objet").style.top=y+"px"
     }
     if ( tecla == "83"){
         y = y + 10
-        this.document.getElementById("object").style.top=y+"px"
+        this.document.getElementById("--objet").style.top=y+"px"
     }
     if ( tecla == "65"){
         x = x - 10
-        this.document.getElementById("object").style.left=x+"px"
+        this.document.getElementById("--objet").style.left=x+"px"
     }
 })
